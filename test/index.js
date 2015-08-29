8eb9fb27a7fc3bb50b5ac8c6a8a72db63580f388
c835722 <c835722@gmail.com>
2015-08-29 11:41:17 +1000

    Update version

--- test/index.js
+++ test/index.js
@@ -3,6 +3,6 @@ import uakari from '../lib';
 
 describe('uakari', function () {
   it('should determine the version of the local packge', function () {
-    assert.equal(uakari.version, '0.0.1', 'expect the current version to be earliest.');
+    assert.equal(uakari.version, '0.0.2', 'expect the current version to be earliest.');
   });
 });
